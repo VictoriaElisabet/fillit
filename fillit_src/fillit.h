d770d8d1e7765f60bc59d6ee76c5588e14fe7cb7
vgrankul <vgrankul@student.hive.fi>
2019-12-04 13:51:01 +0200

    changed Makefile, fixed small spelling errors in fillit.h + adding more libraries, fixed compiling errors in map.c

--- fillit_src/fillit.h
+++ fillit_src/fillit.h
@@ -6,21 +6,26 @@
 /*   By: phakakos <phakakos@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 16:55:37 by phakakos          #+#    #+#             */
-/*   Updated: 2019/12/04 12:57:11 by phakakos         ###   ########.fr       */
+/*   Updated: 2019/12/04 13:47:13 by vgrankul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 
-#include "libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
 
-typedef strutct	s_tetrimino
+typedef struct	s_tetrimino
 {
 	char	c;
 	void	*next;
 	int		arr[4];
-}				t_tetrimino
+}				t_tetrimino;
 
 void	print_error(int i);
 int		map_solve(t_tetrimino *current, int size, char *map, int i);
