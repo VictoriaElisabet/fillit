8248e045b0ac8a5f80e1afd971c7663aa0d6fac0
Petrus Hakakoski <phakakos@c1r3p2.hive.fi>
2019-12-04 14:45:06 +0200

    changes of some kind

--- fillit_src/fillit.h
+++ fillit_src/fillit.h
@@ -6,21 +6,24 @@
 /*   By: phakakos <phakakos@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 16:55:37 by phakakos          #+#    #+#             */
-/*   Updated: 2019/12/04 12:57:11 by phakakos         ###   ########.fr       */
+/*   Updated: 2019/12/04 13:08:40 by phakakos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 
-#include "libft.h"
+# include "libft.h"
+# include <stdlib.h>
+# include <fcntl.h>
 
-typedef strutct	s_tetrimino
+
+typedef struct	s_tetrimino
 {
 	char	c;
 	void	*next;
 	int		arr[4];
-}				t_tetrimino
+}				t_tetrimino;
 
 void	print_error(int i);
 int		map_solve(t_tetrimino *current, int size, char *map, int i);
