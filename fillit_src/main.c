ea478efc0680807044c41d4842e6c957063ddd3a
Petrus Hakakoski <phakakos@c1r3p2.hive.fi>
2019-12-06 15:17:07 +0200

    main norm fixes

--- fillit_src/main.c
+++ fillit_src/main.c
@@ -6,36 +6,35 @@
 /*   By: phakakos <phakakos@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 11:19:44 by vgrankul          #+#    #+#             */
-/*   Updated: 2019/12/06 13:28:13 by vgrankul         ###   ########.fr       */
+/*   Updated: 2019/12/06 15:16:50 by phakakos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-void	print_error(int i)
+void		print_error(int i)
 {
 	if (i == -1)
-		ft_puterr("error");
+		ft_puterr("error\n");
 	else if (i == -2)
-		ft_putstr("usage: fillit input_file");
+		ft_putstr("usage: fillit input_file\n");
 	else if (i == -3)
-		ft_puterr("error: allocation failed");
+		ft_puterr("error: allocation failed\n");
 	exit(0);
 }
 
-char	*join_lines(char *str, char **line)
+static char	*join_lines(char *str, char *line)
 {
 	char *tmp;
 
-	if (!(tmp = ft_strjoin(str, *line)))
+	if (!(tmp = ft_strjoin(str, line)))
 		print_error(-3);
 	ft_strdel(&str);
-	str = tmp;
-	ft_strdel(line);
-	return (str);
+	ft_strdel(&line);
+	return (tmp);
 }
 
-void	check_file(int fd)
+void		check_file(int fd)
 {
 	char		*line;
 	static int	i;
@@ -50,26 +49,27 @@ void	check_file(int fd)
 		j++;
 		if (get_next_line(fd, &line) != 1 || check_line(line) == -1)
 			print_error(-1);
-		str = join_lines(str, &line);
+		str = join_lines(str, line);
 		if (j == 4)
-			str = ((s[i++] = str)) ? NULL : NULL;
+			s[i++] = str;
 	}
-	if (i < 26 && get_next_line(fd, &line) > 0)
-	//need check for if get_next line fails.
+	if (i < 26 && (j = get_next_line(fd, &line)) > 0)
 		ft_strlen(line) == 0 ? check_file(fd) : print_error(-1);
-	else if (i > 26)
+	else if (i > 26 || j == -1)
 		print_error(-1);
 	check_tetrimino_characters(s);
 	check_coordinates(s);
 }
 
-int		main(int argc, char **argv)
+int			main(int argc, char **argv)
 {
 	int fd;
 
 	if (argc < 2 || argc > 2)
 		print_error(-2);
 	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		print_error(-1);
 	check_file(fd);
 	return (0);
 }
